32303ff063c5b919c8f69f2fa544e98ebd4eabf0
pkuanvil-uid1 <119569118+pkuanvil-uid1@users.noreply.github.com>
2023-01-15 23:22:55 +0800

    sanitizeConfg

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const katexMath = require('markdown-it-texmath');
+const _ = require('lodash');
 
 const nbbKaTeX = module.exports;
 
@@ -12,6 +13,32 @@ nbbKaTeX.updateParserRules = (parser) => {
 		katexOptions,
 		delimiters: ['dollars', 'brackets'],
 	});
+};
+
+nbbKaTeX.sanitizeConfig = (config) => {
+	// Search katex source code for 'SvgNode', 'PathNode', 'LineNode', 'makeSvgSpan'
+	const svgTags = [
+		'svg',
+		'line',
+		'path',
+	];
+	const svgAttrs = {
+		line: ['x1', 'y1', 'x2', 'y2', 'stroke-width'],
+		path: ['d'],
+		svg: ['width', 'height', 'style', 'viewBox', 'viewbox', 'preserveAspectRatio', 'preserveaspectratio'],
+	};
+	const svgClasses = {
+		svg: ['hide-tail', 'halfarrow-left', 'halfarrow-right', 'brace-left', 'brace-center', 'brace-right'],
+	};
+
+	config.allowedTags = _.union(config.allowedTags, svgTags);
+	for (const tag of Object.keys(svgAttrs)) {
+		config.allowedAttributes[tag] = _.union(config.allowedAttributes[tag], svgAttrs[tag]);
+	}
+	for (const tag of Object.keys(svgClasses)) {
+		config.allowedClasses[tag] = _.union(config.allowedClasses[tag], svgClasses[tag]);
+	}
+	return config;
 };
 
 nbbKaTeX.getLinkTags = (hookData) => {
